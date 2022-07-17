30c0964935a199b314ae656dcf5721f9afe308a7
Ruslana Pavliuk <pavliuk_r_v@protonmail.com>
2022-07-17 18:34:32 +0300

    home work 22

---
+++ hw22.js
@@ -0,0 +1,30 @@
+// Exercise 5.1
+$('.test').css({'color':'red'});
+
+// Exercise 5.1
+$('h2.test').css({'color': 'red'});
+
+// Exercise 5.3
+$('p').next('p').css({'color': 'red'});
+
+// Exercise 5.4
+$('li.www').next('li').css({'color': 'red'});
+
+// Exercise 5.5
+$('h2 > i').css({'color': 'red'});
+
+// Exercise 5.6
+$('h2').children('i').css({'color': 'red'});
+
+// Exercise 6.1
+$("h2:not('h2.test')").css({'color': 'red'});
+
+// Exercise 6.2
+$(":header:not('.test')").css({'color': 'red'});
+
+// Exercise 6.3
+$(':header').next(':header').css({'color': 'red'});
+
+// Exercise 6.4
+$(':header.test').next(':header.test').css({'color': 'red'});
+
